9ba7bc5415f5e5d6b6bf555b8bea6034b47c69f9
poojacoder123 <agrawalpooja727@gmail.com>
2024-11-05 17:11:22 +0530

    css

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { StarRatingComponent, StarRatingModule } from 'angular-star-rating';
-
+import { StarRatingModule } from 'angular-star-rating';
 import { routes } from './app.routes';
+import { StarRatingComponent } from 'ng-starrating';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    importProvidersFrom( StarRatingModule.forRoot(), StarRatingComponent)
+    importProvidersFrom( StarRatingModule.forRoot())
   ]
 };
--- frontend/src/app/component/pages/home/home.component.ts
+++ frontend/src/app/component/pages/home/home.component.ts
@@ -3,35 +3,32 @@ import { FoodService } from '../../../services/food.service';
 import { Food } from '../../../shared/models/food';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
-
-
+import { StarRatingComponent } from '../../partials/star-rating/star-rating.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, StarRatingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA ],
 })
 export class HomeComponent implements OnInit {
   foods : Food[] = [];
+  
 constructor(private foodService: FoodService){
   
 }
-currentRating = 3;
 
-onRatingChange(newRating: any): void {
-  this.currentRating = newRating;
-  console.log('Rating changed to: ', newRating);
-}
+
+
 ngOnInit(){
   this.foods = this.foodService.getFood()
   console.log(this.foods)
 }
+
+
+ 
 
 
 
