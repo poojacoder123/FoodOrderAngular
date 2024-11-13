27b0c781d34d5b5b835bb997cf644cb7ea9e7f26
poojacoder123 <agrawalpooja727@gmail.com>
2024-11-13 18:02:51 +0530

    cart-page

--- frontend/src/app/component/partials/title/title.component.ts
+++ frontend/src/app/component/partials/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 
 @Component({
@@ -9,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './title.component.scss'
 })
 export class TitleComponent {
-
+ @Input() title!: string;
 }
